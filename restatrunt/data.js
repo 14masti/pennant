d092ade4d02164f700356f83193d44a4d450c4de
Shaik Mastani <137260618+14masti@users.noreply.github.com>
2023-08-16 14:32:26 +0530

    Add files via upload

---
+++ restatrunt/data.js
@@ -0,0 +1,169 @@
+const data = {
+    "biryani_items": [
+    {
+    "name": "Hyderabadi Biryani",
+    "description": "A flavorful and aromatic rice dish made with basmati rice, tender meat (chicken or mutton), and a blend of spices.",
+    "ingredients": ["Basmati rice", "Chicken/Mutton", "Onions", "Yogurt", "Ghee", "Spices"],
+    "spiciness": "Medium",
+    "price": "$12.99",
+    "image_url":"b1.jpg"
+  },
+  {
+    "name": "Lucknowi Biryani",
+    "description": "A traditional biryani from Lucknow, India, made with fragrant rice and slow-cooked meat (usually lamb or goat).",
+    "ingredients": ["Basmati rice", "Lamb/Goat", "Saffron", "Rose water", "Mint leaves", "Spices"],
+    "spiciness": "Mild",
+    "price": "$14.99",
+    "image_url":"b2.jpg"
+  },
+  {
+    "name": "Kolkata Biryani",
+    "description": "A popular biryani variant from Kolkata, India, cooked with potatoes, eggs, and aromatic spices.",
+    "ingredients": ["Basmati rice", "Chicken", "Potatoes", "Eggs", "Caramelized onions", "Spices"],
+    "spiciness": "Medium",
+    "price": "$11.99",
+    "image_url":"b3.jpg"
+  },
+  {
+    "name": "Thalassery Biryani",
+    "description": "A flavorful biryani from Kerala, India, known for its unique blend of spices and the use of Khaima/Jeerakasala rice.",
+    "ingredients": ["Khaima/Jeerakasala rice", "Chicken/Beef", "Caramelized onions", "Coconut milk", "Spices"],
+    "spiciness": "Medium",
+    "price": "$13.99",
+    "image_url":"b4.jpg"
+  },
+  {
+    "name": "Sindhi Biryani",
+    "description": "A delightful biryani from Sindh, Pakistan, prepared with meat (chicken or mutton) and layered with fragrant rice.",
+    "ingredients": ["Basmati rice", "Chicken/Mutton", "Potatoes", "Yogurt", "Tomatoes", "Spices"],
+    "spiciness": "Spicy",
+    "price": "$15.99",
+    "image_url":"b5.jpg"
+  }],
+    "noodle_items": [
+    {
+    "name": "Pad Thai",
+    "description": "A popular Thai stir-fried noodle dish made with rice noodles, shrimp or chicken, bean sprouts, peanuts, and tamarind sauce.",
+    "ingredients": ["Rice noodles", "Shrimp/Chicken", "Bean sprouts", "Tofu", "Peanuts", "Tamarind sauce"],
+    "spiciness": "Mild",
+    "price": "$9.99",
+    "image_url":"n1.jpg"
+  },
+  {
+    "name": "Ramen",
+    "description": "A Japanese noodle soup dish served in a flavorful broth, topped with sliced pork, boiled eggs, nori, and various toppings.",
+    "ingredients": ["Ramen noodles", "Pork", "Eggs", "Nori (seaweed)", "Green onions", "Bamboo shoots"],
+    "spiciness": "Non-spicy",
+    "price": "$10.99",
+    "image_url":"n2.jpg"
+  },
+  {
+    "name": "Pho",
+    "description": "A Vietnamese noodle soup made with rice noodles, beef or chicken, and a fragrant broth with herbs and spices.",
+    "ingredients": ["Rice noodles", "Beef/Chicken", "Bean sprouts", "Thai basil", "Lime", "Hoisin sauce"],
+    "spiciness": "Non-spicy",
+    "price": "$8.99",
+    "image_url":"n3.jpg"
+  },
+  {
+    "name": "Lo Mein",
+    "description": "A Chinese stir-fried noodle dish with vegetables, often served with beef, chicken, shrimp, or tofu.",
+    "ingredients": ["Egg noodles", "Beef/Chicken/Shrimp/Tofu", "Carrots", "Broccoli", "Soy sauce", "Oyster sauce"],
+    "spiciness": "Non-spicy",
+    "price": "$11.99",
+    "image_url":"n4.jpg"
+  },
+  {
+    "name": "Japchae",
+    "description": "A Korean dish made with sweet potato glass noodles stir-fried with vegetables and meat (often beef).",
+    "ingredients": ["Sweet potato glass noodles", "Beef", "Carrots", "Spinach", "Soy sauce", "Sesame oil"],
+    "spiciness": "Non-spicy",
+    "price": "$12.99",
+    "image_url":"n5.jpg"
+  }
+    ],
+    "juice_items": [
+    {
+    "name": "Orange Juice",
+    "description": "A classic and refreshing juice made from freshly squeezed oranges.",
+    "ingredients": ["Fresh oranges"],
+    "sugar_content": "Medium",
+    "price": "$3.99",
+    "image_url":"j1.jpg"
+  },
+  {
+    "name": "Apple Juice",
+    "description": "A sweet and flavorful juice made from freshly pressed apples.",
+    "ingredients": ["Fresh apples"],
+    "sugar_content": "High",
+    "price": "$2.99",
+    "image_url":"j2.jpg"
+  },
+  {
+    "name": "Watermelon Juice",
+    "description": "A hydrating and refreshing juice made from juicy watermelon slices.",
+    "ingredients": ["Fresh watermelon"],
+    "sugar_content": "Low",
+    "price": "$4.49",
+    "image_url":"j3.jpg"
+  },
+  {
+    "name": "Carrot Ginger Juice",
+    "description": "A nutritious juice blend made with fresh carrots and a hint of ginger for added flavor.",
+    "ingredients": ["Fresh carrots", "Fresh ginger"],
+    "sugar_content": "Low",
+    "price": "$4.99",
+    "image_url":"j4.jpg"
+  },
+  {
+    "name": "Pineapple Coconut Juice",
+    "description": "A tropical and creamy juice blend made from fresh pineapples and coconut milk.",
+    "ingredients": ["Fresh pineapples", "Coconut milk"],
+    "sugar_content": "Medium",
+    "price": "$5.99",
+    "image_url":"j5.jpg"
+  }
+    ],
+    "chocolate_items": [
+    {
+    "name": "Milk Chocolate",
+    "description": "A creamy and sweet chocolate made with milk solids and cocoa solids.",
+    "ingredients": ["Sugar", "Cocoa butter", "Milk powder", "Cocoa mass", "Lecithin"],
+    "cocoa_content": "25%",
+    "price": "$1.99",
+    "image_url":"c1.jpg"
+  },
+  {
+    "name": "Dark Chocolate",
+    "description": "A rich and intense chocolate with a higher percentage of cocoa solids and less sugar.",
+    "ingredients": ["Cocoa mass", "Sugar", "Cocoa butter", "Lecithin"],
+    "cocoa_content": "70%",
+    "price": "$2.49",
+    "image_url":"c2.jpg"
+  },
+  {
+    "name": "White Chocolate",
+    "description": "A smooth and creamy chocolate made with cocoa butter, milk solids, and no cocoa solids.",
+    "ingredients": ["Sugar", "Cocoa butter", "Milk powder", "Lecithin"],
+    "cocoa_content": "0%",
+    "price": "$1.79",
+    "image_url":"c3.jpg"
+  },
+  {
+    "name": "Hazelnut Chocolate",
+    "description": "A delightful combination of chocolate and roasted hazelnuts.",
+    "ingredients": ["Sugar", "Cocoa mass", "Cocoa butter", "Hazelnuts", "Lecithin"],
+    "cocoa_content": "40%",
+    "price": "$2.99",
+    "image_url":"c4.jpg"
+  },
+  {
+    "name": "Caramel Sea Salt Chocolate",
+    "description": "A delectable blend of sweet caramel and sea salt with smooth chocolate.",
+    "ingredients": ["Sugar", "Cocoa mass", "Cocoa butter", "Caramel", "Sea salt", "Lecithin"],
+    "cocoa_content": "55%",
+    "price": "$3.49",
+    "image_url":"c5.jpg"
+  }
+    ]
+  };
