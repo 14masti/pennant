4523cb7229713512be8c12802bccf53c2bf990f0
Shaik Mastani <137260618+14masti@users.noreply.github.com>
2023-08-22 14:34:33 +0530

    Update DALclass.java

--- mvc_crud/DALclass.java
+++ mvc_crud/DALclass.java
@@ -7,45 +7,38 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DALclass {
-	Connection conn = null;
-	PreparedStatement preparedStatement =null;
-	Statement st = null;
+	
 	String qry, cs;
-	ResultSet rst;
+
+	public Connection getConnection() {
+        Connection conn = null;
+        try {
+        	String url = "jdbc:postgresql://192.168.110.48:5432/plf_training";
+			String username = "plf_training_admin";
+			String password = "pff123";
+
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+	
+	
 	public ArrayList<employee> fetchRecords(Connection conn,String name)
 	{
 		ArrayList<employee> l1=new ArrayList<>();
+		PreparedStatement preparedStatement =null;
+		ResultSet rst;
 		try {
-			
-			String url = "jdbc:postgresql://192.168.110.48:5432/plf_training";
-			String username = "plf_training_admin";
-			String password = "pff123";
-			
-
-			
-			Class.forName("org.postgresql.Driver");
-			
-			
-			
-			conn=DriverManager.getConnection(url,username,password);
-			
-			
-			//st = conn.createStatement();
-			
-			
 			qry = "select * from resh_emp where ename=?";
-			
 			preparedStatement = conn.prepareStatement(qry);
 			preparedStatement.setString(1, name); 
 			rst = preparedStatement.executeQuery();
-
-			//rst = st.executeQuery(qry);
-			
-			
 			while(rst.next()) {
 				int empno = rst.getInt("empno");
 				String ename = rst.getString("ename");
@@ -54,32 +47,96 @@ public class DALclass {
 				employee e = new employee(empno, ename, job, sal);
 				l1.add(e);
 			}
-	
-			rst.close();
-			
+		rst.close();	
 		}catch(Exception ex)
-	{
+		{
 			ex.printStackTrace();
-		
 		}
 		return l1;	
 	}
-	public void insertEmployee(Connection conn, String empid, String employeeName, String job, String sal) throws NumberFormatException, SQLException {
-		// TODO Auto-generated method stub
+	
+	
+	/*public ArrayList<employee> fetchRecords(Connection conn,String employeeName,int empno,String job,double sal) {
+	    ArrayList<employee> l1 = new ArrayList<>();
+	    PreparedStatement preparedStatement = null;
+	    ResultSet rst;
 
+	    try {
+	        String qry = "SELECT * FROM resh_emp WHERE ename ILIKE ? OR empno::TEXT ILIKE ? OR job ILIKE ? OR sal::TEXT ILIKE ?";
+	        preparedStatement = conn.prepareStatement(qry);
+
+
+	        preparedStatement.setString(1, "%" + employeeName + "%");
+	        preparedStatement.setString(2, "%" + empno + "%");
+	        preparedStatement.setString(3, "%" + job + "%");
+	        preparedStatement.setString(4, "%" + sal + "%");
+
+	        rst = preparedStatement.executeQuery();
+
+	        while (rst.next()) {
+	            int empno1 = rst.getInt("empno");
+	            String ename1 = rst.getString("ename");
+	            String job1 = rst.getString("job");
+	            double sal1 = rst.getDouble("sal");
+	            employee e = new employee(empno1, ename1, job1, sal1);
+	            l1.add(e);
+	        }
+
+	        rst.close();
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+
+	    return l1;
+	}
+*/
+	
+	public void insertEmployee(Connection conn, String empno, String employeeName, String job, String sal) throws NumberFormatException, SQLException {
+		// TODO Auto-generated method stub
+		PreparedStatement preparedStatement =null;
 	        try {
 	            String query = "INSERT INTO resh_emp (empno, ename, job, sal) VALUES (?, ?, ?, ?)";
 	            preparedStatement = conn.prepareStatement(query);
-	            preparedStatement.setInt(1, Integer.parseInt(empid));
+	            preparedStatement.setInt(1, Integer.parseInt(empno));
 	            preparedStatement.setString(2, employeeName);
 	            preparedStatement.setString(3, job);
 	            preparedStatement.setDouble(4, Double.parseDouble(sal));
-
 	            preparedStatement.executeUpdate();
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        } 
 	}
+
+	public ArrayList<employee> fetchAllRecords(Connection conn) {
+	    ArrayList<employee> allRecords = new ArrayList<>();
+	    PreparedStatement preparedStatement = null;
+	    ResultSet rst;
+	    try {
+	        String qry = "SELECT * FROM resh_emp";
+	        preparedStatement = conn.prepareStatement(qry);
+	        rst = preparedStatement.executeQuery();
+	        while (rst.next()) {
+	            int empno = rst.getInt("empno");
+	            String ename = rst.getString("ename");
+	            String job = rst.getString("job");
+	            double sal = rst.getDouble("sal");
+	            employee e = new employee(empno, ename, job, sal);
+	            allRecords.add(e);
+	        }
+	        rst.close();
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	    return allRecords;
+	}
+
+	/*public void deleteEmployee(Connection conn, String empid) throws SQLException {
+	    String query = "DELETE FROM reshu_emp WHERE empid=?";
+	    try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+	        preparedStatement.setInt(1, Integer.parseInt(empid));
+	        preparedStatement.executeUpdate();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}*/
 }
-
-
