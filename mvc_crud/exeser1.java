cf6ca372b44b56aa565e65c9af34857c726ba3b7
Shaik Mastani <137260618+14masti@users.noreply.github.com>
2023-08-22 14:36:06 +0530

    Update exeser1.java

--- mvc_crud/exeser1.java
+++ mvc_crud/exeser1.java
@@ -4,6 +4,7 @@
 package com.reshu;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -24,52 +25,99 @@ public class exeser1 extends HttpServlet {
     }
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		String employeeName = request.getParameter("employeeName");
-		RequestDispatcher RD = null;
-		try {
-			DALclass dal = new DALclass();
-			Connection conn = dal.getConnection(); // Get the connection
-			ArrayList<employee> al = dal.fetchRecords(conn,employeeName);
-			conn.close(); 
-			request.setAttribute("data", al);
-			RD = request.getRequestDispatcher("exe.jsp");
-			RD.forward(request, response);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //response.getWriter().append("Served at: ").append(request.getContextPath());
+       /* String employeeName = request.getParameter("employeeName");
+        int empno = Integer.parseInt(request.getParameter("empno"));
+        String job = request.getParameter("job");
+        double sal = Double.parseDouble(request.getParameter("sal"));
+        RequestDispatcher RD = null;
+        try {
+            DALclass dal = new DALclass();
+            Connection conn = dal.getConnection(); // Get the connection
+            ArrayList<employee> al = dal.fetchRecords(conn, employeeName, empno, job, sal);
+//            conn.close();
+            request.setAttribute("data", al);
+            RD = request.getRequestDispatcher("exe.jsp");
+            RD.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+*/
+	String employeeName = request.getParameter("employeeName");
+	/*int empno = Integer.parseInt(request.getParameter("empno"));
+	String job = request.getParameter("job");
+	double sal = Double.parseDouble(request.getParameter("sal"));*/
+	
+	//RequestDispatcher RD = null;
+	try {
+		DALclass dal = new DALclass();
+		Connection conn = dal.getConnection(); // Get the connection
+		//ArrayList<employee> al = dal.fetchRecords(conn,employeeName,empno,job,sal);
+		ArrayList<employee> al = dal.fetchRecords(conn,employeeName);
+		request.setAttribute("data", al);
+		RequestDispatcher RD = request.getRequestDispatcher("exe.jsp");
+        RD.forward(request, response);
+	} catch (Exception e) {
+		e.printStackTrace();
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-		String action = request.getParameter("action");
-		if (action != null && action.equals("add")) {
-		 // Handle adding new employee record
-		String employeeName = request.getParameter("employeeName");
-		String empid = request.getParameter("empid");
-		String job = request.getParameter("job");
-		String sal = request.getParameter("sal");
+	String action = request.getParameter("action");
 
-		try {
-			DALclass dal = new DALclass();
-			Connection conn = dal.getConnection();
-		    dal.insertEmployee(conn,empid, employeeName, job, sal);
-		    conn.close();
-		    response.sendRedirect("exe.jsp");
-		    } catch (Exception e) {
-		    e.printStackTrace();
-		    }
-			
-		        }
-		    }
+   
+	if (action != null && action.equals("Display")) {
+        // Handle display functionality
+        try {
+            DALclass dal = new DALclass();
+            Connection conn = dal.getConnection();
+            ArrayList<employee> allRecords = dal.fetchAllRecords(conn);
+            //conn.close();
+            request.setAttribute("data1", allRecords);
+            RequestDispatcher RD = request.getRequestDispatcher("exe.jsp");
+            RD.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+        } 
+   
+
+    
+	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response); // Call the common method to process the request
+        
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("ADD")) {
+            String employeeName = request.getParameter("employeeName");
+            String empid = request.getParameter("empno");
+            String job = request.getParameter("job");
+            String sal = request.getParameter("sal");
+
+            try {
+                DALclass dal = new DALclass();
+                Connection conn = dal.getConnection();
+                dal.insertEmployee(conn, empid, employeeName, job, sal);
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } 
+    
+    /*else if (action != null && action.equals("delete")) {
+    String empidToDelete = request.getParameter("empid");
+    try {
+        DALclass dal = new DALclass();
+        Connection conn = dal.getConnection();
+        dal.deleteEmployee(conn, empidToDelete);
+        conn.close();
+        response.sendRedirect("exeser1"); // Redirect to refresh the page
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+} */
+    }
 }
-
-	
-
- 
- 
